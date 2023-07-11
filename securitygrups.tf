83aca34c293d35ea6ed86a667bbe495f0ce0f1fb
krishnanaidu99 <129326410+krishnanaidu99@users.noreply.github.com>
2023-07-11 12:50:39 +0530

    Update securitygrups.tf

--- securitygrups.tf
+++ securitygrups.tf
@@ -9,8 +9,8 @@ resource "aws_security_group" "my-sg" {
  }
  
 ingress {
- from_port  = 8000
-   to_port    = 8000
+ from_port  = 9000
+   to_port    = 9000
    protocol   = "tcp"
    cidr_blocks = ["0.0.0.0/0"]
  }
