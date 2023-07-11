ca5f7d47cac3a0c748f4ef607d962365cfe70667
krishnanaidu99 <129326410+krishnanaidu99@users.noreply.github.com>
2023-07-11 12:49:49 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
- sudo apt update
- sudo apt full-upgrade -y
- sudo apt-get install python3-pip -y
- sudo git clone  https://github.com/krishnanaidu99/fuel.git
- cd  fuel
- sudo pip3 install -r requirements.txt
- sudo python3 app.py
- screen -m -d python3 app.py 
+ #!/bin/bash
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt-get install python3-pip -y
+sudo git clone https://github.com/krishnanaidu99/Abalone-Age-Prediction.git
+cd /
+cd Abalone-Age-Prediction
+pip3 install -r requirements.txt
+python3 app.py
+sudo screen -m -d python3 app.py
