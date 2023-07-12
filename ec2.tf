08d476c0d97c8ebc4280ebe44e96379771d5a8ae
jayanth1521 <116736169+jayanth1521@users.noreply.github.com>
2023-07-12 11:51:52 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,13 @@
-resource "aws_instance" "vamsi" {
+resource "aws_instance" "jay" {
  ami   = "ami-053b0d53c279acc90"
  instance_type = "t2.medium"
- availability_zone  = "us-east-1a"
- key_name = "nani"
+ availability_zone  = "us-east-1"
+ key_name = "awskey"
  vpc_security_group_ids  = [aws_security_group.my-sg.id]
  subnet_id   = aws_subnet.public.id
  associate_public_ip_address = true
  user_data      =  file("userdata.sh")
 tags = {
-  name = "vamsi"
+  name = "jay"
 }
 }
