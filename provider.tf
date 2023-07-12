c6e2273f5a7c83143ce67a4a120dbb41ad7c90d0
krishnanaidu99 <129326410+krishnanaidu99@users.noreply.github.com>
2023-07-12 11:45:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAS2FLQSUJA75DLCFP"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
