d2a747687698508a1d3d93038abba613866224ac
jayanth1521 <116736169+jayanth1521@users.noreply.github.com>
2023-07-12 12:02:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAQOF73UTWFWS2CZ75"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
