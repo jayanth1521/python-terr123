7c0937a286ae0300120c08057b211debb606fc19
jayanth1521 <116736169+jayanth1521@users.noreply.github.com>
2023-07-12 11:57:48 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -2,9 +2,9 @@
 sudo apt update
 sudo apt full-upgrade -y
 sudo apt-get install python3-pip -y
-sudo git clone https://github.com/krishnanaidu99/Abalone-Age-Prediction.git
+sudo git clone https:https://github.com/jayanth1521/fuel.git
 cd /
-cd Abalone-Age-Prediction
+cd fuel
 pip3 install -r requirements.txt
 python3 app.py
 sudo screen -m -d python3 app.py
